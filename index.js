30e6b63c777b97dfbf6336dec579e4d767cde289
Dev Dixit <devdixitsocial@gmail.com>
2026-02-03 15:39:49 +0530

    add app.use options for dep

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.use(cors({
   credentials: true
 }));
 
+app.options("*", cors());
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
